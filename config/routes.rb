2d5125594b828dee1709699b409894d9aa4da971
TheMagicalAubster <aubrey.blairpattison@gmail.com>
2020-07-23 19:54:45 +0000

    user login and user database

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,14 @@ Rails.application.routes.draw do
     resources :categories, except: [:edit, :update, :show]
   end
 
+  get '/login' => 'session#new'
+  post '/login' => 'session#create'
+  get '/logout' => 'session#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+
   
 
   # The priority is based upon order of creation: first created -> highest priority.
