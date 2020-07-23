70557307a16d63ec3a4048a4fa3b15c2b26f6b1e
TheMagicalAubster <aubrey.blairpattison@gmail.com>
2020-07-23 16:02:33 +0000

    about page added with top bar nav

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,17 @@ Rails.application.routes.draw do
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
+  ###
 
+  
   resource :cart, only: [:show] do
     post   :add_item
     post   :remove_item
   end
-
+  
   resources :orders, only: [:create, :show]
-
+  get '/about' => 'about#show'
+  
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
