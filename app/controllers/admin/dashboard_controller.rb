af2d4c2e6482c7cd28437792bd9338628c82463f
TheMagicalAubster <aubrey.blairpattison@gmail.com>
2020-07-23 03:03:39 +0000

    admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,8 @@
 class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with :name => ENV["username"], :password => ENV["password"]
+  # http_basic_authenticate_with :name => ENV["username"], :password => ENV["password"]
 
   def show
+    @product_count = Product.all.count
+    @category_count = Category.all.count
   end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,8 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all.order(created_at: :desc)
+    @product_count = Product.all.count
+    
   end
 
   def show
